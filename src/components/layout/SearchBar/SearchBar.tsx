3776cf101a4dd7f0fe3241a141ad6ce98233c53b
Tuxi4k <kaserka.caserca@gmail.com>
2026-04-30 14:52:31 +0500

    feat: add SearchBar

---
+++ src/components/layout/SearchBar/SearchBar.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import { MenuIcon, SearchIcon } from "lucide-react";
+import styles from "./SearchBar.module.css";
+
+export const SearchBar = () => {
+  return (
+    <div className={clsx(styles.searchBar)}>
+      <button className={styles.button}>
+        <MenuIcon className={styles.buttonIcon} />
+      </button>
+      <input
+        type="search"
+        placeholder="Найти что-то для рыбалки..."
+        className={clsx(styles.searchInput)}
+      />
+      <button className={styles.button}>
+        <SearchIcon className={styles.buttonIcon} />
+      </button>
+    </div>
+  );
+};
