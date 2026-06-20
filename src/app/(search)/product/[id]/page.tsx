17a5967e1cb6ed21e9efc8b11dc262e841ab684f
FantomStudy <arzamastsevdaniil@gmail.com>
2026-06-21 00:58:17 +0500

    feat: implement product recommendations section with responsive styles

--- src/app/(search)/product/[id]/page.tsx
+++ src/app/(search)/product/[id]/page.tsx
@@ -1,5 +1,37 @@
+import { ProductCard } from "@/components/ProductCard";
+import { ProductRecomentaions } from "@/components/ProductRecomendations";
+import { ScrollCarousel, Typography } from "@/components/ui";
+import { getProducts } from "@/constants/mock/product";
+import styles from "./page.module.css";
+
 const ProductPage = () => {
-  return <main className="container"></main>;
+  const recommendedProducts = getProducts();
+
+  return (
+    <main className="container">
+      <div className={styles.wrapper}>
+        <div>
+          <Typography variant="text-2xl" tag="h2" className={styles.title}>
+            Рекомендуем также
+          </Typography>
+
+          <ScrollCarousel>
+            {recommendedProducts.map((product) => (
+              <ProductCard key={product.id} product={product} className={styles.productCard} />
+            ))}
+          </ScrollCarousel>
+        </div>
+
+        <div>
+          <Typography variant="text-2xl" tag="h2" className={styles.title}>
+            Возможно вам понравится
+          </Typography>
+
+          <ProductRecomentaions />
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default ProductPage;
